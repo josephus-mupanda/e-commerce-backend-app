0025b3504c4a7369a12ae3fd6cae256ffb829f25
josephus <josephusmupanda48@gmail.com>
2025-10-19 17:09:25 +0200

    add start-docker

---
+++ start-docker.ps1
@@ -0,0 +1,13 @@
+# Add Docker Toolbox to PATH
+$env:PATH += ";C:\Program Files\Docker Toolbox"
+
+# Set Docker Machine environment
+& "C:\Program Files\Docker Toolbox\docker-machine.exe" env --shell powershell default | Invoke-Expression
+
+# Navigate to project
+cd "C:\Users\josep\Development\My projects\e-commerce-backend-app"
+
+# Start Docker Compose
+docker-compose -f docker-compose.yml up -d
+
+Write-Host "Docker services started. You can now run Spring Boot."
