046bc88428ca8351b9a35838f0b836049a7ee31f
josephus <josephusmupanda48@gmail.com>
2025-10-19 16:56:51 +0200

    this is my e-commerce-backend app

---
+++ src/main/java/com/josephus/e_commerce_backend_app/ECommerceBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.josephus.e_commerce_backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josephus/e_commerce_backend_app/ECommerceBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephus.e_commerce_backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
